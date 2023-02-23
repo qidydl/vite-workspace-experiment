d9f807337cc0de7f6cf1e2f34cc62f490450e0cc
David Osolkowski <qidydl@gmail.com>
2023-02-23 18:08:01 -0500

    Resolve some issues

--- shared/vite.config.ts
+++ shared/vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    //NOTE: tsconfigPaths is required for vitest to resolve paths properly, even though vite itself is fine
     plugins: [react(), tsconfigPaths()],
     test: {
         globals: true,
