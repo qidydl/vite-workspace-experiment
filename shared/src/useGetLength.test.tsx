f30530ce22d368b183e5fd59b6736378cfdc7023
David Osolkowski <qidydl@gmail.com>
2023-02-23 17:47:46 -0500

    Get tests working

--- shared/src/useGetLength.test.tsx
+++ shared/src/useGetLength.test.tsx
@@ -1,10 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { api } from "api";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useGetLength } from "useGetLength";
 
 function runHook(uri: string) {
-    const qc = new QueryClient();
+    //NOTE: We should always disable retries for test performance
+    const qc = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false,
+            },
+        },
+    });
     const { result } = renderHook(() => useGetLength(uri), {
         wrapper: ({ children }) => <QueryClientProvider client={qc}>{children}</QueryClientProvider>,
     });
@@ -12,12 +19,14 @@ function runHook(uri: string) {
 }
 
 describe("useGetLength Hook", () => {
-    it("does something", () => {
-        //const getLengthMock = vi.spyOn(api, "getLength").mockResolvedValue(123);
+    it("behaves normally", async () => {
+        const getLengthMock = vi.spyOn(api, "getLength").mockResolvedValue(123);
 
         const result = runHook("https://test");
 
+        await waitFor(() => expect(result.current.isSuccess).toBeTruthy());
+        expect(getLengthMock).toHaveBeenCalledOnce();
+        expect(getLengthMock).toHaveBeenCalledWith("https://test", expect.any(AbortSignal));
         expect(result.current.data).toBe(123);
-        //expect(getLengthMock).toHaveBeenCalledOnce();
     });
 });
