ef7f60140c6e3354850cc503c0eef7662d00b2df
David Osolkowski <qidydl@gmail.com>
2023-02-23 10:12:47 -0500

    Enable https via ASP.NET Core certs

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Experimenting with creating a web UI using Vite, workspaces, and Preact or React
 - [x] Set up library (tsc only) and app (Vite)
 - [ ] Set up tests for both library and app
 - [ ] Set up Prettier, ESLint, Stylelint
+  - Could do them in Vite: https://bitbucket.org/unimorphic/vite-plugin-linter/src/master/
+  - Another option: https://github.com/fi3ework/vite-plugin-checker
 - [ ] Set up automated pipeline that builds, runs linting, etc.
 - [ ] Can we push most/all deps up to root package.json so they're consistent across the repo?
   - No, but see https://github.com/JamieMason/syncpack which also has more linting
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,7 +1,73 @@
 import { defineConfig } from "vite";
+import { spawn, SpawnOptions } from "child_process";
+import path from "path";
+import fs from "fs";
 import react from "@vitejs/plugin-react";
 
+function spawnAsync(
+  command: string,
+  args: readonly string[],
+  options: SpawnOptions
+) {
+  return new Promise<void>((resolve, reject) => {
+    const process = spawn(command, args, options);
+    process.on("error", reject);
+    process.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`child exited with code ${code}`));
+      }
+    });
+  });
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async ({ command, mode }) => {
+  let https = undefined;
+
+  if (command === "serve") {
+    // Set up HTTPS for the application using HTTPS certificates from ASP.NET Core
+
+    // Base folder set based on Windows or Linux
+    const baseFolder = process.env.APPDATA
+      ? `${process.env.APPDATA}/ASP.NET/https`
+      : `${process.env.HOME}/.aspnet/https`;
+
+    const certFilePath = path.join(baseFolder, "qidydl-app.pem");
+    const keyFilePath = path.join(baseFolder, "qidydl-app.key");
+
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+      await spawnAsync(
+        "dotnet",
+        [
+          "dev-certs",
+          "https",
+          "--export-path",
+          certFilePath,
+          "--format",
+          "Pem",
+          "--no-password",
+        ],
+        {
+          stdio: "inherit",
+        }
+      );
+    }
+
+    https = {
+      cert: fs.readFileSync(certFilePath),
+      key: fs.readFileSync(keyFilePath),
+    };
+
+    console.log("Using HTTPS via", certFilePath);
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5000,
+      https,
+    },
+  };
 });
