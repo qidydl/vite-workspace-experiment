8f2fe4f3e6036425a797e8e2d7580c25e21aff69
David Osolkowski <qidydl@gmail.com>
2023-02-23 19:33:00 -0500

    Get tests working

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -12,5 +12,7 @@ describe("The application", () => {
         renderComponent();
 
         expect(screen.getByText(/Click on the Vite and React logos to learn more/i)).toBeVisible();
+        expect(screen.getByText(/This is a shared component\./)).toBeVisible();
+        expect(screen.getByText(/This is a shared component too with hot-reloading changes/)).toBeVisible();
     });
 });
